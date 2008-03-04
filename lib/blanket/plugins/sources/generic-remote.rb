d4e20ec7c0d343066f5d65d2af5965f38e896c56
Jim Van Fleet <jim@jimvanfleet.com>
2008-03-03 22:10:53 -0500

    Adding a generic remote source

---
+++ lib/blanket/plugins/sources/generic-remote.rb
@@ -0,0 +1,51 @@
+class GenericRemote < Source
+  
+  def initialize(reader)
+    @reader = reader
+  end
+  
+  def self.attribute_symbols
+    [:source_type, :host, :user, :password, :remote_path, :local_path ]
+  end
+  
+  def self.default_source_type
+    "Confluence"
+  end
+  
+  def self.default_host
+    "yourhost.com"
+  end
+  
+  def self.default_user
+    "username"
+  end
+  
+  def self.default_password
+    "password"
+  end
+  
+  def self.default_remote_path
+    "/path/to/remote/target/file"
+  end
+  
+  def self.default_local_path
+    "/path/to/local/blanket"
+  end
+    
+  def remote_backup_path
+    remote_path
+  end
+  
+  def local_backup_path
+    local_path
+  end
+  
+  def prep_command
+    noop
+  end
+  
+  def cleanup_command
+    noop
+  end
+  
+end
