cee0d8b03e5699e0e6f7ce30cade8a292ebbff9b
Jim Van Fleet <jim@jimvanfleet.com>
2008-04-18 00:43:58 -0400

    We've got to be running out of these things, right?  I'll be RDoc'ing as I go in the future.  If I happen to know what I'm doing at the time.

--- lib/blanket/plugins/sources/subversion.rb
+++ lib/blanket/plugins/sources/subversion.rb
@@ -1,54 +1,70 @@
+# Source for backing up Subversion repository.
+#
+# Create a Subversion source by running <code>blanket-cfg</code> with the
+# <code>-o Subversion</code> option.  Your directory will be compressed
+# using a <code>tar</code> and <code>gzip</code> command. 
+# 
+# In the source.yml file, set the following options:
+#
+# [user] Username on the remote system.
+# [password] Password on the remote system.
+# [host] Domain name of the remote system.
+# [repository] Remote directory containing the repository
+# [remote_path] The name of the file on the remote machine you'd like the repository to be backed up to.
+# [local_path] The location on client running the blanket where the database backup is stored before being sent to a sink.
+#
 class Subversion < Source
   
-  def initialize(reader)
+  def initialize(reader) #:nodoc:
     @reader = reader
   end
   
-  def self.attribute_symbols
+  def self.attribute_symbols #:nodoc:
     [:source_type, :host, :user, :password, :repository, :remote_path, :local_path ]
   end
   
-  def self.default_source_type
+  def self.default_source_type #:nodoc:
     "Subversion"
   end
   
-  def self.default_host
+  def self.default_host #:nodoc:
     "yourhost.com"
   end
   
-  def self.default_user
+  def self.default_user #:nodoc:
     "username"
   end
   
-  def self.default_password
+  def self.default_password #:nodoc:
     "password"
   end
   
-  def self.default_repository
+  def self.default_repository #:nodoc:
     "/path/to/repo"
   end
   
-  def self.default_remote_path
+  def self.default_remote_path #:nodoc:
     "/path/to/remote/backup-file"
   end
   
-  def self.default_local_path
+  def self.default_local_path #:nodoc:
     "/path/to/local/backup-file"
   end
     
-  def remote_backup_path
+  def remote_backup_path #:nodoc:
     remote_path
   end
   
-  def local_backup_path
+  def local_backup_path #:nodoc:
     local_path
   end
   
-  def prep_command
+  def prep_command #:nodoc:
     "svnadmin dump #{repository} > #{remote_backup_path}"
   end
   
-  def cleanup_command
+  # Ugh, tired of not having these things written!
+  def cleanup_command #:nodoc:
     noop
   end
   
